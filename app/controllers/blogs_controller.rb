9e509c41fcec52770f99690e614b8990d457e99b
septem <qitengyuxi@gmail.com>
2021-10-08 23:54:29 +0900

    init

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,70 @@
+class BlogsController < ApplicationController
+  before_action :set_blog, only: %i[ show edit update destroy ]
+
+  # GET /blogs or /blogs.json
+  def index
+    @blogs = Blog.all
+  end
+
+  # GET /blogs/1 or /blogs/1.json
+  def show
+  end
+
+  # GET /blogs/new
+  def new
+    @blog = Blog.new
+  end
+
+  # GET /blogs/1/edit
+  def edit
+  end
+
+  # POST /blogs or /blogs.json
+  def create
+    @blog = Blog.new(blog_params)
+    if params[:back]
+      render :new
+    else
+      if @blog.save
+        redirect_to blogs_path, notice: "ブログを作成しました！"
+      else
+        render :new
+      end
+    end
+  end
+
+  # PATCH/PUT /blogs/1 or /blogs/1.json
+  def update
+    @blog = Blog.find(params[:id])
+    if @blog.update(blog_params)
+      redirect_to blogs_path, notice: "ブログを編集しました！"
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /blogs/1 or /blogs/1.json
+  def destroy
+    @blog.destroy
+    respond_to do |format|
+      format.html { redirect_to blogs_url, notice: "Blog was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  def confirm
+    @blog = Blog.new(blog_params)
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_blog
+      @blog = Blog.find(params[:id])
+      render :new if @blog.invalid?
+    end
+
+    # Only allow a list of trusted parameters through.
+    def blog_params
+      params.require(:blog).permit(:title, :content)
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  if Rails.env.development?
+      mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+    end
+  post "/graphql", to: "graphql#execute"
+  resources :blogs do
+    collection do
+      post :confirm
+    end
+  end
+end
